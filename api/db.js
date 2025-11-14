0589d65fba36fdeb5517d937ad12c9b5ebf39ec0
goldedgelabs <goldedgelabs@gmail.com>
2025-11-14 19:03:32 +0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,19 +1,27 @@
+// api/db.js
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGODB_URI;
 
-let cached = global.__mongo;
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("Please define MONGO_URI in your environment variables");
+}
+
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.__mongo = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connect() {
   if (cached.conn) return cached.conn;
+  
   if (!cached.promise) {
-    const opts = {};
-    cached.promise = mongoose.connect(MONGO_URI, opts).then(m => {
-      return m;
-    });
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    };
+    cached.promise = mongoose.connect(MONGO_URI, opts).then(mongoose => mongoose);
   }
   cached.conn = await cached.promise;
   return cached.conn;
